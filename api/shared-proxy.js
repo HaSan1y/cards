56b37bb16f34efd984cbcccc29c1aadd5759b078
Hasan Yildirim <hadirekt@gmail.com>
2025-04-17 01:36:07 +0200

    test

--- api/shared-proxy.js
+++ api/shared-proxy.js
@@ -1,20 +1,25 @@
 const fetch = require("node-fetch");
 
 module.exports = async function getData(type) {
-	if (type === "joke") {
-		const response = await fetch("https://www.yomama-jokes.com/api/v1/jokes/random/");
-		return await response.json();
-	} else if (type === "insult") {
-		const response = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json");
-		return await response.json();
-	} else if (type === "image") {
-		const response = await fetch("https://picsum.photos/200/300");
-		const arrayBuffer = await response.arrayBuffer();
-		return {
-			buffer: Buffer.from(arrayBuffer),
-			contentType: response.headers.get("content-type") || "image/jpeg",
-		};
-	} else {
-		throw new Error("Unknown type");
+	try {
+		if (type === "joke") {
+			const response = await fetch("https://www.yomama-jokes.com/api/v1/jokes/random/");
+			return await response.json();
+		} else if (type === "insult") {
+			const response = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json");
+			return await response.json();
+		} else if (type === "image") {
+			const response = await fetch("https://picsum.photos/200/300");
+			const arrayBuffer = await response.arrayBuffer();
+			return {
+				buffer: Buffer.from(arrayBuffer),
+				contentType: response.headers.get("content-type") || "image/jpeg",
+			};
+		} else {
+			throw new Error("Unknown type");
+		}
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		throw error;
 	}
 };
