1a1d92156600010b4658440827596e89e7bcc72a
Hasan Yildirim <hadirekt@gmail.com>
2025-04-17 08:19:09 +0200

    vercel serverless server doesnt work 4 me

--- api/shared-proxy.js
+++ api/shared-proxy.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 
 module.exports = async function getData(type) {
+	console.log(`Incoming ${req.method} to ${req.url}`);
 	try {
 		if (type === "joke") {
 			const response = await fetch("https://www.yomama-jokes.com/api/v1/jokes/random/");
--- api/vercel-proxy.js
+++ api/vercel-proxy.js
@@ -1,29 +1,39 @@
 const getData = require("./shared-proxy.js");
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
+	console.log("Vercel function called!");
 	// Example: /api/vercel-proxy?type=joke or /api/vercel-proxy?type=insult or ?type=image
 	const { type } = req.query;
 
 	try {
 		if (type === "image") {
 			const { buffer, contentType } = await getData("image");
+			res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 			res.setHeader("Content-Type", contentType);
 			res.setHeader("Access-Control-Allow-Origin", "*");
 			res.status(200).end(buffer);
 		} else if (type === "insult") {
 			const data = await getData("insult");
+			res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.status(200).json(data);
+		} else if (type === "joke") {
+			const data = await getData("joke");
+			res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 			res.setHeader("Access-Control-Allow-Origin", "*");
 			res.status(200).json(data);
 		} else {
-			// Default to joke
-			const data = await getData("joke");
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.status(200).json(data);
+			console.error("Invalid type:", type);
+			res.status(400).json({ error: "Invalid type" });
 		}
 	} catch (error) {
+		console.error("Error fetching data:", error);
 		res.status(500).json({ error: "Failed fetching data" });
 	}
-};
+}
 // /api/vercel-proxy?type=joke
 // /api/vercel-proxy?type=insult
 // /api/vercel-proxy?type=image
